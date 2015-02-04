e82661e0016af68c2342bd62b563e7631119c613
zhangmyihua <michaelzhang2@msn.com>
2015-02-04 09:50:35 -0500

    Added pictures to each individual url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
-
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
+end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -57,4 +58,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
